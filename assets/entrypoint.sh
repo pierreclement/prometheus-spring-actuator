5c785d3c18ce211f9677a4e0fdc940b517bb170e
Steve Coffman <steve.coffman@ithaka.org>
2017-12-04 11:02:53 -0500

    Readability improvements in script

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -7,10 +7,16 @@ fi
 
 if [ "$JMX" = true ]; then
   printf "Running the application in JMX mode\n"
-  JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=$JMX_PORT -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+
+  JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote \
+  -Dcom.sun.management.jmxremote.port=$JMX_PORT \
+  -Dcom.sun.management.jmxremote.local.only=false \
+  -Dcom.sun.management.jmxremote.authenticate=false \
+  -Dcom.sun.management.jmxremote.ssl=false"
 fi
 printf "Running APP_HOME/JAR_FILE $APP_HOME/$JAR_FILE\n"
 # Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
 # See here https://docs.docker.com/engine/reference/builder/#/entrypoint
 exec java $JAVA_OPTS \
-       -Djava.security.egd=file:/dev/./urandom -jar $APP_HOME/$JAR_FILE
+       -Djava.security.egd=file:/dev/./urandom \
+       -jar $APP_HOME/$JAR_FILE
